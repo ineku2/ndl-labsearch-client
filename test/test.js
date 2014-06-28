c95cef1c7d792d7298c34a07170c9e205091e96f
Tsunekawa Mao <tsunekaw@slis.tsukuba.ac.jp>
2014-06-28 19:22:02 +0900

    add test cases for Ajax.Manager#executeRequest

--- test/test.js
+++ test/test.js
@@ -22,12 +22,14 @@ require([
   var expect = chai.expect;
   chai.should();
 
-  var Manager = new AjaxSolr.Manager({
-                    solrUrl: settings.labs.solrUrl
-  });
-
+  
   //テストケース
   describe('AjaxSolr.Manager', function (){
+    var Manager;
+
+    beforeEach(function (){
+      Manager = new AjaxSolr.Manager({solrUrl: settings.labs.solrUrl });
+    });
 
     it('is object', function (){
       expect(Manager).to.be.a('object');
@@ -57,6 +59,63 @@ require([
       it('return an array', function (){
 	expect(correct_case()).to.be.instanceof(Array);
       });
+    });
+
+    describe('#executeRequest', function (){
+      var server, success, failure;
+
+      beforeEach(function (){
+	sinon.stub($, 'ajax', function (options) {
+	  var d = $.Deferred();
+	  if(options.success){
+	    d.done(options.success({
+	      status_code: 200,
+	      data: {
+	        response: {},
+	        facet_counts: {}
+	      }
+	    }));
+	  }
+	  if(options.error){
+	    d.fail(options.error);
+	  }
+	  d.success = d.done;
+	  d.error   = d.fail;
+	  return d;
+	});
+
+	success = sinon.spy();
+	failure = sinon.spy();
+      });
+
+      afterEach(function (){
+	$.ajax.restore();
+      });
+
+      it('should return jQuery Deffered Object', function (){
+	var result = Manager.executeRequest('/select', 'q=*%3A*', success, failure);
+	expect(result).to.be.instanceof(Object);
+	expect(result.done).to.be.a('function');
+	expect(result.fail).to.be.a('function');
+	expect(result.resolve).to.be.a('function');
+	expect(result.reject).to.be.a('function');
+      });
+
+      it('call jQuery.ajax', function (){
+	Manager.executeRequest('/select', 'q=*%3A*', success, failure);
+	expect($.ajax.calledOnce).to.be.true;
+      });
+
+      it('call success handler when success', function (){
+	Manager.executeRequest('/select', 'q=*%3A*', success, failure).resolve();
+	expect(success.calledOnce).to.be.true;
+      });
+
+      it('call failure handler when failure', function (){
+	Manager.executeRequest('/select', 'q=*%3A*', success, failure).reject();
+	expect(failure.calledOnce).to.be.true;
+      });
+
     });
 
   });
